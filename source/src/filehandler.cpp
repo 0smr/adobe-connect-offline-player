9fa27acaad745f14b0a6a57c20be3c3f8a5a9df0
SMR <seyyedmortezarazavi76@gmail.com>
2021-03-03 06:13:11 +0330

    moved function bodies to source file.

--- source/src/filehandler.cpp
+++ source/src/filehandler.cpp
@@ -1,1 +1,85 @@
 #include "filehandler.h"
+
+fileHandler::fileHandler(QObject *parent) : QObject(parent)
+{
+
+}
+
+void fileHandler::skipUntil(QXmlStreamReader &xmlReader, QString elementName, QString breakIfElement)
+{
+    xmlReader.readNextStartElement();
+    while(xmlReader.name() != elementName &&
+           xmlReader.name() != breakIfElement &&
+           !xmlReader.atEnd())
+        xmlReader.readNextStartElement();
+}
+
+QString fileHandler::getElementValue(QXmlStreamReader &xmlReader, QString elementName, QString breakIfElement)
+{
+    skipUntil(xmlReader,elementName,breakIfElement);
+    if(xmlReader.name() == breakIfElement)
+        return "";
+    xmlReader.readNext();
+    return xmlReader.text().toString();
+}
+
+QVariantMap fileHandler::extractDataStreamTimeStamps(QUrl fileAddr)
+{
+    QFile indexStream(fileAddr.toLocalFile());
+    QVariantMap     tempSession;
+    QVariantList    dataStreamList;
+
+    if (!indexStream.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug()  << "cant open file";
+        return QVariantMap();
+    }
+
+    QXmlStreamReader xmlReader(&indexStream);
+
+    tempSession["root"] = QFileInfo(indexStream).path();
+
+    while (!xmlReader.atEnd())
+    {
+        skipUntil(xmlReader,"Message","");
+
+        QString method = getElementValue(xmlReader,"Method");
+        QString objectName = getElementValue(xmlReader,"name","String");
+        //if Method wasn't playEvent and object name wasn't StreamManagerId_Mainstream skip operation.
+        if(method != "playEvent" && objectName != "StreamManagerId_Mainstream")
+        {
+            xmlReader.readNext();
+            // if reach end of indexStream set session duration.
+            if(xmlReader.text().toString() == "__stop__")
+            {
+                tempSession["duration"] = getElementValue(xmlReader,"Number");
+            }
+
+            continue;
+        }
+
+        qreal time = getElementValue(xmlReader,"time").toLong();
+        QString status = getElementValue(xmlReader,"String");
+
+        if(status == "streamRemoved")
+        {
+            QVariantMap tds;
+            tds["endTimeStamp"]     = time;
+            tds["startTimeStamp"]   = getElementValue(xmlReader,"startTime");
+            tds["name"]             = getElementValue(xmlReader,"streamName");
+            tds["senderId"]         = getElementValue(xmlReader,"streamPublisherID");
+            tds["type"]             = getElementValue(xmlReader,"streamType");
+
+            dataStreamList.append(tds);
+        }
+    }
+    tempSession["dataStreamList"] = dataStreamList;
+    sessions.append(tempSession);
+
+    return tempSession;
+}
+
+bool fileHandler::isValidFolder(QUrl folderUrl)
+{
+    return QFile::exists(folderUrl.toLocalFile() + "/indexstream.xml");
+}
--- source/src/filehandler.h
+++ source/src/filehandler.h
@@ -10,96 +10,52 @@
 
 #include <QDebug>
 
-//#include "session.h"
+#include <zlib.h>
 
 class fileHandler : public QObject
 {
     Q_OBJECT
 public:
-    explicit fileHandler(QObject *parent = nullptr) : QObject(parent)
-    {
+    explicit fileHandler(QObject *parent = nullptr);
 
-    }
-
-    void skipUntil(QXmlStreamReader &xmlReader,QString elementName, QString breakIfElement)
-    {
-        xmlReader.readNextStartElement();
-        while(xmlReader.name() != elementName &&
-              xmlReader.name() != breakIfElement &&
-              !xmlReader.atEnd())
-            xmlReader.readNextStartElement();
-    }
-
+    void skipUntil(QXmlStreamReader &xmlReader,QString elementName, QString breakIfElement);
     QString getElementValue(QXmlStreamReader &xmlReader,
                                QString elementName,
-                               QString breakIfElement = "Message")
-    {
-        skipUntil(xmlReader,elementName,breakIfElement);
-        if(xmlReader.name() == breakIfElement)
-            return "";
-        xmlReader.readNext();
-        return xmlReader.text().toString();
-    }
-
-    Q_INVOKABLE QVariantMap extractTimeStamps(QUrl fileAddr)
-    {
-        QFile indexStream(fileAddr.toLocalFile());
-        QVariantMap     tempSession;
-        QVariantList    dataStreamList;
-
-        if (!indexStream.open(QIODevice::ReadOnly | QIODevice::Text))
-        {
-            qDebug()  << "cant open file";
-            return QVariantMap();
-        }
-
-        QXmlStreamReader xmlReader(&indexStream);
-
-        tempSession["root"] = QFileInfo(indexStream).path();
-
-        while (!xmlReader.atEnd())
-        {
-            skipUntil(xmlReader,"Message","");
-
-            QString method = getElementValue(xmlReader,"Method");
-            QString objectName = getElementValue(xmlReader,"name","String");
-            //if Method wasn't playEvent and object name wasn't StreamManagerId_Mainstream skip operation.
-            if(method != "playEvent" && objectName != "StreamManagerId_Mainstream")
-            {
-                xmlReader.readNext();
-                // if reach end of indexStream set session duration.
-                if(xmlReader.text().toString() == "__stop__")
-                {
-                    tempSession["duration"] = getElementValue(xmlReader,"Number");
-                }
-
-                continue;
-            }
-
-            qreal time = getElementValue(xmlReader,"time").toLong();
-            QString status = getElementValue(xmlReader,"String");
-
-            if(status == "streamRemoved")
-            {
-                QVariantMap tds;
-                tds["endTimeStamp"]     = time;
-                tds["startTimeStamp"]   = getElementValue(xmlReader,"startTime");
-                tds["name"]             = getElementValue(xmlReader,"streamName");
-                tds["senderId"]         = getElementValue(xmlReader,"streamPublisherID");
-                tds["type"]             = getElementValue(xmlReader,"streamType");
-
-                dataStreamList.append(tds);
-            }
-        }
-        tempSession["dataStreamList"] = dataStreamList;
-        sessions.append(tempSession);
-
-        return tempSession;
-    }
+                               QString breakIfElement = "Message");
+    Q_INVOKABLE QVariantMap extractDataStreamTimeStamps(QUrl fileAddr);
 
 signals:
 
 public slots:
+    bool isValidFolder(QUrl folderUrl);
+    bool decompressZipFile(QUrl workDirectory)
+    {
+        //Open the ZIP archive
+        int err = 0;
+        zip *z = zip_open("foo.zip", 0, &err);
+
+        //Search for the file of given name
+        const char *name = "file.txt";
+        struct zip_stat st;
+        zip_stat_init(&st);
+        zip_stat(z, name, 0, &st);
+
+        //Alloc memory for its uncompressed contents
+        char *contents = new char[st.size];
+
+        //Read the compressed file
+        zip_file *f = zip_fopen(z, name, 0);
+        zip_fread(f, contents, st.size);
+        zip_fclose(f);
+
+        //And close the archive
+        zip_close(z);
+
+        //Do something with the contents
+        //delete allocated memory
+        delete[] contents;
+        return false;
+    }
 
 private:
     QVariantList sessions;
