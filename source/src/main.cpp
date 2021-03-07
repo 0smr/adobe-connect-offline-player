486bd6f537f7a49bc2b8dff57e43f6150ad33a35
SMR <seyyedmortezarazavi76@gmail.com>
2021-03-07 21:10:00 +0330

    added application version and other pieces of information.

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,6 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QDebug>
+#include <QIcon>
 
 #include "filehandler.h"
 #include "networkhandler.h"
@@ -14,6 +15,12 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<fileHandler>    ("io.file",     1,0,"FileHandler");
     qmlRegisterType<networkHandler> ("io.network",  1,0,"NetworkHandler");
+
+    app.setOrganizationName     ("smr");
+    app.setApplicationVersion   ("0.1");
+    app.setOrganizationDomain   ("smr67.github.io");
+    app.setApplicationName      ("Adobe Connect Offline Player");
+    app.setWindowIcon(QIcon(":/Resources/Application Icon/ACOP.ico"));
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
